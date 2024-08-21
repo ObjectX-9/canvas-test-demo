ebd8c9e6d3b3838d9cb10cb1bd5d2cff5346af11
zhuling822 <ling.zhu@js.design>
2024-08-21 15:17:18 +0800

    fix: publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CanvasContainer from "./components/CanvasContainer";
 import { initJsdState } from "./core/init/init";
 
 function App() {
+  const [isInitialized, setIsInitialized] = useState(false);
+
   useEffect(() => {
     initJsdState({})
-  }, [])
+    setIsInitialized(true);
+  }, []);
+
+  if (!isInitialized) {
+    return <div>Loading...</div>; // 显示加载状态
+  }
+
   return (
     <>
       <CanvasContainer />
