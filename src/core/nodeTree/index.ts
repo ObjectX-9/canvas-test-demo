887e004597731df7fc1565686731dbc7a3a1e345
zhuling904 <ling.zhu.904@gmail.com>
2025-09-23 23:17:48 +0800

    feat: 创建节点 & 节点树

--- src/core/nodeTree/index.ts
+++ src/core/nodeTree/index.ts
@@ -5,13 +5,34 @@ import { PageState } from "../types/nodes/page";
 import { BaseNode } from "./node/baseNode";
 import { Rectangle } from "./node/rectangle";
 import { Page } from "./node/page";
+import { elementStore } from "../store/ElementStore";
 
 export class NodeTree {
-  private nodes: Map<string, BaseState> = new Map();
+  private nodes: Map<string, BaseNode> = new Map();
 
   addNode(nodeState: BaseState) {
     if (!this.nodes.has(nodeState.id)) {
-      this.nodes.set(nodeState.id, nodeState);
+      // 根据 nodeState 类型创建对应的节点对象
+      let node: BaseNode;
+
+      switch (nodeState.type) {
+        case "rectangle": {
+          node = new Rectangle(nodeState as RectangleState);
+          break;
+        }
+        case "page": {
+          node = new Page(nodeState as PageState);
+          break;
+        }
+        default:
+          throw new Error(`Unsupported node type: ${nodeState.type}`);
+      }
+
+      // 将节点对象添加到节点树
+      this.nodes.set(nodeState.id, node);
+
+      // 将 nodeState 添加到 elementStore
+      elementStore.addElement(nodeState.id, nodeState);
     }
   }
 
@@ -31,18 +52,8 @@ export class NodeTree {
 
   createAllElements(elements: ElementCollections) {
     Object.values(elements).forEach((elemState) => {
-      switch (elemState.type) {
-        case "rectangle": {
-          const rect = new Rectangle(elemState as RectangleState);
-          this.addNode(rect);
-          break;
-        }
-        case "page": {
-          const page = new Page(elemState as PageState);
-          this.addNode(page);
-          break;
-        }
-      }
+      // 直接使用重构后的 addNode 方法
+      this.addNode(elemState);
     });
   }
 }
